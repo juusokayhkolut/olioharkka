e62ed3828f88e52dc3ff9359ead82f9153ae56f2
juusopikapay <juuso@pikapay.fi>
2024-03-25 09:45:43 +0200

    Commit 2

--- app/src/main/java/com/example/olioharkka/MainActivity.java
+++ app/src/main/java/com/example/olioharkka/MainActivity.java
@@ -6,7 +6,7 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
-    //hello
+    //hello 2
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
